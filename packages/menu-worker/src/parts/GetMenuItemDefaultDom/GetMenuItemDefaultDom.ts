177be1bbf9baab93ddbba226dc977e3436c5b6f6
Le Vivilet <levivilet@pm.me>
2025-11-08 13:43:08 +0100

    feature: simplify parsekey

--- packages/menu-worker/src/parts/GetMenuItemDefaultDom/GetMenuItemDefaultDom.ts
+++ packages/menu-worker/src/parts/GetMenuItemDefaultDom/GetMenuItemDefaultDom.ts
@@ -1,9 +1,9 @@
+import { ParseKey } from '@lvce-editor/constants'
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import type { VisibleMenuItem } from '../VisibleMenuItem/VisibleMenuItem.ts'
 import * as AriaRoles from '../AriaRoles/AriaRoles.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as GetKeyBindingsString from '../GetKeyBindingsString/GetKeyBindingsString.ts'
-import * as ParseKey from '../ParseKey/ParseKey.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
 
