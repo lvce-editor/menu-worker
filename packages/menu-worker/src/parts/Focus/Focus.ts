7da64fa49a7d3e109541e15ed62d2839651c3985
Le Vivilet <levivilet@pm.me>
2025-11-08 10:24:05 +0100

    feature: remove unused code (#7)

--- packages/menu-worker/src/parts/Focus/Focus.ts
+++
@@ -1,5 +0,0 @@
-import * as ParentRpc from '../ParentRpc/ParentRpc.ts'
-
-export const setFocus = async (focusKey: number): Promise<void> => {
-  await ParentRpc.invoke('Focus.setFocus', focusKey)
-}
