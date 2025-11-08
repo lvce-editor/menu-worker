ba71d7e84e496d3dd6e09f21f2542deadf27dcc8
Le Vivilet <levivilet@pm.me>
2025-11-08 12:41:10 +0100

    feature: update menu module

--- packages/menu-worker/src/parts/Menu/Menu.ts
+++ packages/menu-worker/src/parts/Menu/Menu.ts
@@ -1,5 +1,9 @@
 // TODO lazyload menuEntries and use Command.execute (maybe)
+import * as GetMenuEntriesWithKeyBindings from '../GetMenuEntriesWithKeyBindings/GetMenuEntriesWithKeyBindings.ts'
+import * as GetMenuVirtualDom from '../GetMenuVirtualDom/GetMenuVirtualDom.ts'
+import * as GetVisibleMenuItems from '../GetVisibleMenuItems/GetVisibleMenuItems.ts'
 import * as MenuItemFlags from '../MenuItemFlags/MenuItemFlags.ts'
+import * as RendererProcess from '../RendererProcess/RendererProcess.ts'
 
 export const MENU_WIDTH = 150
 
@@ -66,8 +70,124 @@ export const getIndexToFocusNext = (menu: any): any => {
   return getIndexToFocusNextStartingAt(menu.items, startIndex)
 }
 
+interface State {
+  menus: any[]
+  latestTimeStamp: number
+  enterTimeout: number
+}
+export const state: State = {
+  menus: [],
+  latestTimeStamp: 0,
+  enterTimeout: -1,
+}
+
+export const addMenuInternal = (menu: any): any => {
+  if (state.menus.length > 5) {
+    throw new Error('too many menus')
+  }
+  state.menus.push(menu)
+  return menu
+}
+
 // TODO handle printable letter and focus item that starts with that letter
 
 // TODO pageup / pagedown keys
 
 // TODO more tests
+
+const CONTEXT_MENU_ITEM_HEIGHT = 26
+const CONTEXT_MENU_SEPARATOR_HEIGHT = 11
+const CONTEXT_MENU_PADDING = 8
+
+export const getMenuHeight = (items: readonly any[]): number => {
+  let height = CONTEXT_MENU_PADDING
+  for (const item of items) {
+    switch (item.flags) {
+      case MenuItemFlags.Separator:
+        height += CONTEXT_MENU_SEPARATOR_HEIGHT
+        break
+      default:
+        height += CONTEXT_MENU_ITEM_HEIGHT
+        break
+    }
+  }
+  return height
+}
+
+const getMenuBounds = (x: number, y: number, items: readonly any[]): any => {
+  const menuWidth = CONTEXT_MENU_WIDTH
+  const menuHeight = getMenuHeight(items)
+  const layoutState = { points: [0, 0] }
+  const windowWidth = layoutState.points[0]
+  const windowHeight = layoutState.points[1]
+  // TODO maybe only send labels and keybindings to ui (id not needed on ui)
+  // TODO what about separators?
+
+  if (x + menuWidth > windowWidth) {
+    x -= menuWidth
+  }
+  if (y + menuHeight > windowHeight) {
+    y -= menuHeight
+  }
+
+  return {
+    x,
+    y,
+    width: menuWidth,
+    height: menuHeight,
+  }
+}
+
+export const show = async (x: number, y: number, id: any, mouseBlocking = false, ...args: readonly any[]): Promise<void> => {
+  const items = await GetMenuEntriesWithKeyBindings.getMenuEntriesWithKeyBindings(id, ...args)
+  const bounds = getMenuBounds(x, y, items)
+  const menu = addMenuInternal({
+    id,
+    items,
+    focusedIndex: -1,
+    level: state.menus.length,
+    x: bounds.x,
+    y: bounds.y,
+  })
+  const visible = GetVisibleMenuItems.getVisible(menu.items, -1, false, menu.level)
+  const dom = GetMenuVirtualDom.getMenuVirtualDom(visible).slice(1)
+  await RendererProcess.invoke(
+    /* Menu.show */ 'Menu.showMenu',
+    /* x */ bounds.x,
+    /* y */ bounds.y,
+    /* width */ bounds.width,
+    /* height */ bounds.height,
+    /* items */ menu.items,
+    /* level */ menu.level,
+    /* parentIndex */ -1,
+    /* dom */ dom,
+    /* mouseBlocking */ mouseBlocking,
+  )
+}
+
+export const show2 = async (uid: number, menuId: any, x: number, y: number, mouseBlocking = false, ...args: readonly any[]): Promise<any> => {
+  const items = await GetMenuEntriesWithKeyBindings.getMenuEntriesWithKeyBindings2(uid, menuId, ...args)
+  const bounds = getMenuBounds(x, y, items)
+  const menu = addMenuInternal({
+    id: menuId,
+    items,
+    focusedIndex: -1,
+    level: state.menus.length,
+    x: bounds.x,
+    y: bounds.y,
+  })
+  const visible = GetVisibleMenuItems.getVisible(menu.items, -1, false, menu.level)
+  const dom = GetMenuVirtualDom.getMenuVirtualDom(visible).slice(1)
+  await RendererProcess.invoke(
+    /* Menu.show */ 'Menu.showMenu',
+    /* x */ bounds.x,
+    /* y */ bounds.y,
+    /* width */ bounds.width,
+    /* height */ bounds.height,
+    /* items */ menu.items,
+    /* level */ menu.level,
+    /* parentIndex */ -1,
+    /* dom */ dom,
+    /* mouseBlocking */ mouseBlocking,
+  )
+}
