0cd25584dea42391f72dc2110bc48a35b420bbec
Le Vivilet <levivilet@pm.me>
2025-11-08 12:49:15 +0100

    feature: update listen function (#23)

--- packages/menu-worker/src/parts/Listen/Listen.ts
+++ packages/menu-worker/src/parts/Listen/Listen.ts
@@ -1,10 +1,10 @@
 import { WebWorkerRpcClient } from '@lvce-editor/rpc'
+import { RendererWorker } from '@lvce-editor/rpc-registry'
 import * as CommandMap from '../CommandMap/CommandMap.ts'
-import * as ParentRpc from '../ParentRpc/ParentRpc.ts'
 
 export const listen = async (): Promise<void> => {
   const rpc = await WebWorkerRpcClient.create({
     commandMap: CommandMap.commandMap,
   })
-  ParentRpc.set(rpc)
+  RendererWorker.set(rpc)
 }
