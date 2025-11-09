db93ead782f7cc4a819b7213c41e38b82d6138f5
Le Vivilet <levivilet@pm.me>
2025-11-09 20:56:07 +0100

    feature: add hide (#67)

---
+++ packages/menu-worker/src/parts/Hide/Hide.ts
@@ -0,0 +1,10 @@
+import { RendererWorker } from '@lvce-editor/rpc-registry'
+import { getMenuHideCommands } from '../GetMenuHideCommands/GetMenuHideCommands.ts'
+
+export const hide = async (restoreFocus = true): Promise<void> => {
+  const { commands } = await getMenuHideCommands(restoreFocus)
+  if (commands.length > 0) {
+    // @ts-ignore
+    await RendererWorker.invoke(...commands)
+  }
+}
