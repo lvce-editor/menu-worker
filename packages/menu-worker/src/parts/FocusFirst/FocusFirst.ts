1cb0f2c1d8563044d15a4ffa53d39cbc3b995b67
Le Vivilet <levivilet@pm.me>
2025-11-09 21:00:31 +0100

    feature: add focusFirst

---
+++ packages/menu-worker/src/parts/FocusFirst/FocusFirst.ts
@@ -0,0 +1,35 @@
+import { canBeFocused } from '../CanBeFocused/CanBeFocused.ts'
+import { focusIndex } from '../FocusIndex/FocusIndex.ts'
+import { getAll, getCount } from '../InternalMenuState/InternalMenuState.ts'
+import * as Logger from '../Logger/Logger.js'
+
+const getCurrentMenu = (): any => {
+  if (getCount() === 0) {
+    Logger.warn('menu not available')
+  }
+  return getAll().at(-1)
+}
+
+export const getIndexToFocusNextStartingAt = (items: any, startIndex: number): number => {
+  for (let i = startIndex; i < startIndex + items.length; i++) {
+    const index = i % items.length
+    const item = items[index]
+    if (canBeFocused(item)) {
+      return index
+    }
+  }
+  return -1
+}
+
+export const getIndexToFocusFirst = (items: readonly any[]): number => {
+  return getIndexToFocusNextStartingAt(items, 0)
+}
+
+export const focusFirst = async (): Promise<void> => {
+  const menu = getCurrentMenu()
+  if (getCount() === 0) {
+    return
+  }
+  const indexToFocus = getIndexToFocusFirst(menu.items)
+  await focusIndex(menu, indexToFocus)
+}
