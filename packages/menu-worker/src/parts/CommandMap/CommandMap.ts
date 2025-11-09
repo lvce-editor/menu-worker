b98d8e27eca5c7340068fc33b5fbc49a76330681
Le Vivilet <levivilet@pm.me>
2025-11-09 21:02:02 +0100

    feature: register submenu commands (#66)
    * feature: register submenu commands
    
    * move
    
    * import

--- packages/menu-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/menu-worker/src/parts/CommandMap/CommandMap.ts
@@ -6,11 +6,15 @@ import { getMenuHideCommands } from '../GetMenuHideCommands/GetMenuHideCommands.
 import * as GetMenuIds from '../GetMenuIds/GetMenuIds.ts'
 import { getMenuShowCommands } from '../GetMenuShowCommands/GetMenuShowCommands.ts'
 import * as HandleContextMenu from '../HandleContextMenu/HandleContextMenu.ts'
+import { handleMouseEnter } from '../HandleMouseEnter/HandleMouseEnter.ts'
+import { handleMouseLeave } from '../HandleMouseLeave/HandleMouseLeave.ts'
+import { hideSubMenus } from '../HideSubMenus/HideSubMenus.ts'
 import * as LoadContent from '../LoadContent/LoadContent.ts'
 import * as MenuEntries from '../MenuEntries/MenuEntries.ts'
 import * as Render2 from '../Render2/Render2.ts'
 import * as RenderEventListeners from '../RenderEventListeners/RenderEventListeners.ts'
 import * as SaveState from '../SaveState/SaveState.ts'
+import { showSubMenu } from '../ShowSubMenu/ShowSubMenu.ts'
 import * as WrapCommand from '../WrapCommand/WrapCommand.ts'
 
 export const commandMap = {
@@ -29,4 +33,8 @@ export const commandMap = {
   'Menu.show2': ContextMenu.show2,
   'Menu.getShowCommands': getMenuShowCommands,
   'Menu.getHideCommands': getMenuHideCommands,
+  'Menu.showSubMenu': showSubMenu,
+  'Menu.hideSubMenus': hideSubMenus,
+  'Menu.handleMouseEnter': handleMouseEnter,
+  'Menu.handleMouseLeave': handleMouseLeave,
 }
---
+++ packages/menu-worker/src/parts/HandleMouseEnter/HandleMouseEnter.ts
@@ -0,0 +1,43 @@
+/* eslint-disable @typescript-eslint/no-implied-eval */
+import { focusIndex } from '../FocusIndex/FocusIndex.ts'
+import { hideSubMenus } from '../HideSubMenus/HideSubMenus.ts'
+import { get, getCount, getLatestTimestamp, setTimestamp } from '../InternalMenuState/InternalMenuState.ts'
+import { showSubMenuAtEnter } from '../ShowSubMenu/ShowSubMenu.ts'
+
+const MENU_DELAY_TRIANGLE = 300
+
+const resolveAfterTimeout = (fn: any): void => {
+  setTimeout(fn, MENU_DELAY_TRIANGLE)
+}
+
+export const handleMouseEnter = async (level: number, index: number, enterX: number, enterY: number, enterTimeStamp: number): Promise<any> => {
+  setTimestamp(enterTimeStamp)
+  if (level >= getCount()) {
+    return
+  }
+  const menu = get(level)
+  if (menu.focusedIndex === index) {
+    return
+  }
+  if (level < getCount() - 1) {
+    const subMenu = get(level + 1)
+    const subMenuEnterX = subMenu.enterX
+    // TODO should check for triangle position here
+    if (enterX >= subMenuEnterX) {
+      await new Promise(resolveAfterTimeout)
+      if (getLatestTimestamp() !== enterTimeStamp) {
+        return
+      }
+    }
+  }
+  const item = menu.items[index]
+  await focusIndex(menu, index)
+  switch (item.flags) {
+    case /* SubMenu */ 4:
+      await showSubMenuAtEnter(menu.level, index, enterX, enterY)
+      break
+    default:
+      await hideSubMenus(menu.level)
+      break
+  }
+}
