e792df2364469712431c601f526a2edf65a75bc6
Le Vivilet <levivilet@pm.me>
2025-11-09 20:57:26 +0100

    feature: add selectIndex (#68)

---
+++ packages/menu-worker/src/parts/SelectIndex/SelectIndex.ts
@@ -0,0 +1,50 @@
+import { MenuItemFlags } from '@lvce-editor/constants'
+import * as ExecuteMenuItemCommand from '../ExecuteMenuItemCommand/ExecuteMenuItemCommand.js'
+import { hide } from '../Hide/Hide.ts'
+import { get } from '../InternalMenuState/InternalMenuState.ts'
+import { showSubMenu } from '../ShowSubMenu/ShowSubMenu.ts'
+
+const selectIndexNone = async (menu: any, item: any): Promise<void> => {
+  await Promise.all([hide(/* restoreFocus */ false), ExecuteMenuItemCommand.executeMenuItemCommand(item)])
+}
+
+const selectIndexRestoreFocus = async (menu: any, item: any): Promise<void> => {
+  await Promise.all([hide(/* restoreFocus */ true), ExecuteMenuItemCommand.executeMenuItemCommand(item)])
+}
+
+const selectIndexSubMenu = async (menu: any, item: any, index: number): Promise<void> => {
+  if (menu.focusedIndex === index) {
+    return
+  }
+  await showSubMenu(menu.level, menu.focusedIndex)
+}
+
+const selectIndexDefault = async (): Promise<void> => {}
+
+const selectIndexIgnore = async (menu: any, item: any): Promise<void> => {
+  await ExecuteMenuItemCommand.executeMenuItemCommand(item)
+}
+
+const getSelectIndexFunction = (flags: number): any => {
+  switch (flags) {
+    case MenuItemFlags.None:
+      return selectIndexNone
+    case MenuItemFlags.SubMenu:
+      return selectIndexSubMenu
+    case MenuItemFlags.RestoreFocus:
+      return selectIndexRestoreFocus
+    case MenuItemFlags.Ignore:
+      return selectIndexIgnore
+    default:
+      return selectIndexDefault
+  }
+}
+
+export const selectIndex = async (level: number, index: number): Promise<any> => {
+  const menu = get(level)
+  // TODO avoid assignment
+  menu.focusedIndex = index
+  const item = menu.items[menu.focusedIndex]
+  const fn = getSelectIndexFunction(item.flags)
+  await fn(menu, item, index)
+}
