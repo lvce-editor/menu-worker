8c28d139c94dab32ed33ae981bbd70ec57c33533
Le Vivilet <levivilet@pm.me>
2025-11-23 17:22:55 +0100

    feature: move menu bounds (#92)

--- packages/menu-worker/src/parts/GetMenuShowCommands/GetMenuShowCommands.ts
+++ packages/menu-worker/src/parts/GetMenuShowCommands/GetMenuShowCommands.ts
@@ -1,10 +1,8 @@
 // TODO lazyload menuEntries and use Command.execute (maybe)
+import { getMenuBounds } from '../GetMenuBounds/GetMenuBounds.ts'
 import * as GetMenuVirtualDom from '../GetMenuVirtualDom/GetMenuVirtualDom.ts'
 import * as GetVisibleMenuItems from '../GetVisibleMenuItems/GetVisibleMenuItems.ts'
 import { addMenuInternal, getAll, getCount } from '../InternalMenuState/InternalMenuState.ts'
-import * as MenuItemFlags from '../MenuItemFlags/MenuItemFlags.ts'
-
-const CONTEXT_MENU_WIDTH = 250
 
 // TODO difference between focusing with mouse or keyboard
 // with mouse -> open submenu
@@ -17,51 +15,6 @@ const CONTEXT_MENU_WIDTH = 250
 // TODO pageup / pagedown keys
 
 // TODO more tests
-
-const CONTEXT_MENU_ITEM_HEIGHT = 26
-const CONTEXT_MENU_SEPARATOR_HEIGHT = 11
-const CONTEXT_MENU_PADDING = 8
-
-const getMenuHeight = (items: readonly any[]): number => {
-  let height = CONTEXT_MENU_PADDING
-  for (const item of items) {
-    switch (item.flags) {
-      case MenuItemFlags.Separator:
-        height += CONTEXT_MENU_SEPARATOR_HEIGHT
-        break
-      default:
-        height += CONTEXT_MENU_ITEM_HEIGHT
-        break
-    }
-  }
-  return height
-}
-
-const getMenuBounds = (x: number, y: number, items: readonly any[]): any => {
-  const menuWidth = CONTEXT_MENU_WIDTH
-  const menuHeight = getMenuHeight(items)
-  const layoutState = { points: [0, 0] }
-  const windowWidth = layoutState.points[0]
-  const windowHeight = layoutState.points[1]
-  // TODO maybe only send labels and keybindings to ui (id not needed on ui)
-  // TODO what about separators?
-
-  if (x + menuWidth > windowWidth) {
-    x -= menuWidth
-    x = Math.max(x, 0)
-  }
-  if (y + menuHeight > windowHeight) {
-    y -= menuHeight
-    y = Math.max(y, 0)
-  }
-
-  return {
-    x,
-    y,
-    width: menuWidth,
-    height: menuHeight,
-  }
-}
 
 export const getMenuShowCommands = async (items: any, menuId: any, x: number, y: number, mouseBlocking = false): Promise<any> => {
   const bounds = getMenuBounds(x, y, items)
