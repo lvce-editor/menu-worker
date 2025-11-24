18ad424d11e69316402f66cf39ee24dd8d3f5afc
Le Vivilet <levivilet@pm.me>
2025-11-24 22:28:22 +0100

    fix: menu hover

--- packages/menu-worker/src/parts/ShowSubMenu/ShowSubMenu.ts
+++ packages/menu-worker/src/parts/ShowSubMenu/ShowSubMenu.ts
@@ -1,3 +1,4 @@
+import { RendererProcess } from '@lvce-editor/rpc-registry'
 import { getMenuHeight } from '../GetMenuHeight/GetMenuHeight.ts'
 import { getMenuVirtualDom } from '../GetMenuVirtualDom/GetMenuVirtualDom.ts'
 import { getVisible } from '../GetVisibleMenuItems/GetVisibleMenuItems.ts'
@@ -5,7 +6,7 @@ import { addMenuInternal, get, getAll, getCount, set } from '../InternalMenuStat
 import { getMenuWidth, MENU_WIDTH } from '../Menu/Menu.ts'
 import { getMenuEntries } from '../MenuEntries/MenuEntries.ts'
 
-export const showSubMenuAtEnter = async (level: number, index: number, enterX: number, enterY: number): Promise<any> => {
+export const showSubMenuAtEnter = async (level: number, index: number, enterX: number, enterY: number): Promise<void> => {
   // TODO delete old menus
   set(getAll().slice(0, level + 1))
   const parentMenu = get(level)
@@ -25,22 +26,19 @@ export const showSubMenuAtEnter = async (level: number, index: number, enterX: n
   const height = getMenuHeight(subMenuItems)
   const visible = getVisible(subMenu.items, -1, false, subMenu.level)
   const dom = getMenuVirtualDom(visible).slice(1)
-  return {
-    commands: [
-      /* Menu.showMenu */ 'Menu.showMenu',
-      /* x */ subMenu.x,
-      /* y */ subMenu.y,
-      /* width */ width,
-      /* height */ height,
-      /* items */ subMenu.items,
-      /* level */ subMenu.level,
-      /* parentIndex */ index,
-      /* dom */ dom,
-    ],
-    menus: getAll(),
-  }
+  await RendererProcess.invoke(
+    /* Menu.showMenu */ 'Menu.showMenu',
+    /* x */ subMenu.x,
+    /* y */ subMenu.y,
+    /* width */ width,
+    /* height */ height,
+    /* items */ subMenu.items,
+    /* level */ subMenu.level,
+    /* parentIndex */ index,
+    /* dom */ dom,
+  )
 }
 
-export const showSubMenu = async (level: number, index: number): Promise<any> => {
+export const showSubMenu = async (level: number, index: number): Promise<void> => {
   return showSubMenuAtEnter(level, index, -1, -1)
 }
