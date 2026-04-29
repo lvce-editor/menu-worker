a584656f3e053a3b1d5eeb06431ba37de87f4357
Le Vivilet <levivilet@pm.me>
2026-04-29 23:21:29 +0200

    feat: refactor listen function to utilize initializeRendererWorker

--- packages/menu-worker/src/parts/Listen/Listen.ts
+++ packages/menu-worker/src/parts/Listen/Listen.ts
@@ -1,12 +1,7 @@
-import { WebWorkerRpcClient } from '@lvce-editor/rpc'
-import { RendererWorker } from '@lvce-editor/rpc-registry'
-import * as CommandMap from '../CommandMap/CommandMap.ts'
 import { initializeRendererProcess } from '../InitializeRendererProcess/InitializeRendererProcess.ts'
+import { initializeRendererWorker } from '../InitializeRendererWorker/InitializeRendererWorker.ts'
 
 export const listen = async (): Promise<void> => {
-  const rpc = await WebWorkerRpcClient.create({
-    commandMap: CommandMap.commandMap,
-  })
-  RendererWorker.set(rpc)
+  await initializeRendererWorker()
   await initializeRendererProcess()
 }
