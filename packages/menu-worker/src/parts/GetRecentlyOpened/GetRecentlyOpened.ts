6ee16d9edfcbf232c41db897071fe0d6d3735a3a
Le Vivilet <levivilet@pm.me>
2026-06-30 21:21:38 +0200

    feature: add separate statement (#152)

--- packages/menu-worker/src/parts/GetRecentlyOpened/GetRecentlyOpened.ts
+++ packages/menu-worker/src/parts/GetRecentlyOpened/GetRecentlyOpened.ts
@@ -1,5 +1,6 @@
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 
-export const getRecentlyOpened = (): Promise<any> => {
-  return RendererWorker.invoke(/* RecentlyOpened.getRecentlyOpened */ 'RecentlyOpened.getRecentlyOpened')
+export const getRecentlyOpened = async (): Promise<any> => {
+  const recentlyOpened = await RendererWorker.invoke(/* RecentlyOpened.getRecentlyOpened */ 'RecentlyOpened.getRecentlyOpened')
+  return recentlyOpened
 }
