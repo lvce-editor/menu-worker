0cc075f2e027452e7441aa8077e07930e64e9c97
Le Vivilet <levivilet@pm.me>
2026-07-19 14:45:09 +0000

    feature: enable recommended virtual dom lint rules

--- eslint.config.js
+++ eslint.config.js
@@ -3,9 +3,12 @@ import * as actions from '@lvce-editor/eslint-plugin-github-actions'
 
 export default [
   ...config.default,
+  ...config.recommendedVirtualDom,
   ...actions.default,
   {
     rules: {
+      'jest/expect-expect': 'off',
+      'jest/no-disabled-tests': 'off',
       'jest/no-restricted-jest-methods': 'off',
       '@cspell/spellchecker': 'off',
       'sonarjs/assertions-in-tests': 'off',
@@ -17,6 +20,22 @@ export default [
       'unicorn/prefer-https': 'off',
       'unicorn/prefer-minimal-ternary': 'off',
       'unicorn/prefer-promise-with-resolvers': 'off',
+    },
+  },
+  {
+    files: [
+      'packages/menu-worker/src/parts/InternalMenuState/InternalMenuState.ts',
+      'packages/menu-worker/src/parts/KeyBindingsState/KeyBindingsState.ts',
+      'packages/menu-worker/src/parts/NativeHostState/NativeHostState.ts',
+    ],
+    rules: {
+      'virtual-dom/prefer-state-destructuring': 'off',
+    },
+  },
+  {
+    files: ['packages/menu-worker/test/**/*.ts'],
+    rules: {
+      'virtual-dom/prefer-merge-class-names': 'off',
     },
   },
 ]
--- packages/menu-worker/src/parts/GetMenuItemDisabledDom/GetMenuItemDisabledDom.ts
+++ packages/menu-worker/src/parts/GetMenuItemDisabledDom/GetMenuItemDisabledDom.ts
@@ -2,13 +2,14 @@ import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import type { VisibleMenuItem } from '../VisibleMenuItem/VisibleMenuItem.ts'
 import * as AriaRoles from '../AriaRoles/AriaRoles.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
 
 const disabled: VirtualDomNode = {
   ariaDisabled: true,
   childCount: 1,
-  className: `${ClassNames.MenuItem} ${ClassNames.MenuItemDisabled}`,
+  className: MergeClassNames.mergeClassNames(ClassNames.MenuItem, ClassNames.MenuItemDisabled),
   disabled: true,
   role: AriaRoles.MenuItem,
   tabIndex: -1,
