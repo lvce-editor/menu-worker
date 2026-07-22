31a5eeb5e0758e1c46f56c36a7a35b9f0d842bd8
Le Vivilet <levivilet@pm.me>
2026-07-22 05:46:39 +0200

    chore: update eslint and eslint config to latest (#165)

--- packages/menu-worker/src/parts/GetMenuItemDefaultDom/GetMenuItemDefaultDom.ts
+++ packages/menu-worker/src/parts/GetMenuItemDefaultDom/GetMenuItemDefaultDom.ts
@@ -4,14 +4,21 @@ import type { VisibleMenuItem } from '../VisibleMenuItem/VisibleMenuItem.ts'
 import * as AriaRoles from '../AriaRoles/AriaRoles.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as GetKeyBindingsString from '../GetKeyBindingsString/GetKeyBindingsString.ts'
+import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
+
+const keyBindingNode: VirtualDomNode = {
+  childCount: 1,
+  className: ClassNames.MenuItemKeyBinding,
+  type: VirtualDomElements.Span,
+}
 
 export const getMenuItemDefaultDom = (menuItem: VisibleMenuItem): readonly VirtualDomNode[] => {
   const { isFocused, key, label } = menuItem
   let className = ClassNames.MenuItem
   if (isFocused) {
-    className += ' ' + ClassNames.MenuItemFocused
+    className = MergeClassNames.mergeClassNames(className, ClassNames.MenuItemFocused)
   }
   const dom: any[] = [
     {
@@ -27,14 +34,7 @@ export const getMenuItemDefaultDom = (menuItem: VisibleMenuItem): readonly Virtu
     dom[0].childCount++
     const parsedKey = ParseKey.parseKey(key)
     const keyBindingsString = GetKeyBindingsString.getKeyBindingString(parsedKey.key, false, parsedKey.isCtrl, parsedKey.isShift, false)
-    dom.push(
-      {
-        childCount: 1,
-        className: ClassNames.MenuItemKeyBinding,
-        type: VirtualDomElements.Span,
-      },
-      text(keyBindingsString),
-    )
+    dom.push(keyBindingNode, text(keyBindingsString))
   }
   return dom
 }
--- packages/menu-worker/src/parts/GetMenuItemNoopDom/GetMenuItemNoopDom.ts
+++ packages/menu-worker/src/parts/GetMenuItemNoopDom/GetMenuItemNoopDom.ts
@@ -1,13 +1,12 @@
 import { VirtualDomElements } from '@lvce-editor/constants'
-import { text } from '@lvce-editor/virtual-dom-worker'
+import { text, type VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import type { MenuItemRenderer } from '../MenuItemRenderer/MenuItemRenderer.ts'
 
+const unsupportedMenuItemNode: VirtualDomNode = {
+  childCount: 1,
+  type: VirtualDomElements.Div,
+}
+
 export const getMenuItemsNoopDom: MenuItemRenderer = () => {
-  return [
-    {
-      childCount: 1,
-      type: VirtualDomElements.Div,
-    },
-    text(`Unsupport menu item type`),
-  ]
+  return [unsupportedMenuItemNode, text(`Unsupport menu item type`)]
 }
