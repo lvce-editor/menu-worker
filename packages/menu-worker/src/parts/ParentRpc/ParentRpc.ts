48eba843720fc45bc8e2be584756b19eb91dbf8d
Le Vivilet <levivilet@pm.me>
2026-08-01 10:57:16 -0700

    feature: update eslint dependencies (#169)

--- packages/menu-worker/src/parts/ParentRpc/ParentRpc.ts
+++ packages/menu-worker/src/parts/ParentRpc/ParentRpc.ts
@@ -1,3 +1,10 @@
+import type { Rpc } from '@lvce-editor/rpc'
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 
-export const { invoke, set } = RendererWorker
+export const invoke = (method: string, ...params: readonly unknown[]): Promise<unknown> => {
+  return RendererWorker.invoke(method, ...params)
+}
+
+export const set = (rpc: Rpc): void => {
+  RendererWorker.set(rpc)
+}
