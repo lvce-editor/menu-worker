1e298a5f082e5710e72fcc476498d58528e148f4
levivilet <72156503+levivilet@users.noreply.github.com>
2026-09-06 12:38:03 +0000

    fix: open submenus when selecting their menu item

--- packages/menu-worker/src/parts/SelectIndexSubMenu/SelectIndexSubMenu.ts
+++ packages/menu-worker/src/parts/SelectIndexSubMenu/SelectIndexSubMenu.ts
@@ -1,8 +1,5 @@
 import { showSubMenu } from '../ShowSubMenu/ShowSubMenu.ts'
 
 export const selectIndexSubMenu = async (menu: any, item: any, index: number): Promise<void> => {
-  if (menu.focusedIndex === index) {
-    return
-  }
-  await showSubMenu(menu.level, menu.focusedIndex)
+  await showSubMenu(menu.level, index)
 }
