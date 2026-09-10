ff0f44cc237bea1c2b15ec72a19a0b171e0c8c97
Le Vivilet <levivilet@pm.me>
2026-09-10 06:59:01 +0200

    fix: display readable SSH paths in open recent (#181)

--- packages/menu-worker/test/PathDisplay.test.ts
+++ packages/menu-worker/test/PathDisplay.test.ts
@@ -32,3 +32,15 @@ test('getTitle with empty homeDir', () => {
   expect(PathDisplay.getTitle('', '/path/to/file')).toBe('/path/to/file')
   expect(PathDisplay.getTitle('', 'file:///path/to/file')).toBe('/path/to/file')
 })
+test('getTitle displays remote SSH paths and hosts', () => {
+  expect(PathDisplay.getTitle('/home/user', 'remote-ssh://dev/home/user/project')).toBe('/home/user/project [SSH: dev]')
+  expect(PathDisplay.getTitle('', 'remote-ssh://dev:2222/work/my%20project')).toBe('/work/my project [SSH: dev:2222]')
+  expect(PathDisplay.getTitle('', 'remote-ssh://[::1]/work')).toBe('/work [SSH: [::1]]')
+  expect(PathDisplay.getTitle('', 'remote-ssh://dev')).toBe('/ [SSH: dev]')
+})
+
+test('getTitle preserves malformed remote SSH uris', () => {
+  for (const uri of ['remote-ssh://[', 'remote-ssh://dev/%ZZ', 'remote-ssh:///work']) {
+    expect(PathDisplay.getTitle('', uri)).toBe(uri)
+  }
+})
