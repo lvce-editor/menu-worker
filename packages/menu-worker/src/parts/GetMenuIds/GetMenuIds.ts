c2bd5230509e4d9c5f7a5142eca3b4e7f6a550c7
levivilet <72156503+levivilet@users.noreply.github.com>
2026-09-11 03:46:15 +0000

    fix: route legacy menu lookup through submenu dispatcher

--- packages/menu-worker/src/parts/GetMenuIds/GetMenuIds.ts
+++ packages/menu-worker/src/parts/GetMenuIds/GetMenuIds.ts
@@ -1,13 +1,16 @@
+import { MenuEntryId } from '@lvce-editor/constants'
+import type { MenuEntry } from '../MenuEntry/MenuEntry.ts'
+import { getTitleBarMenuEntries } from '../GetTitleBarMenuEntries/GetTitleBarMenuEntries.ts'
+import * as MenuEntriesTitleBar from '../MenuEntriesTitleBar/MenuEntriesTitleBar.ts'
 import { menus } from '../Menus/Menus.ts'
 
 export const getMenuIds = (): readonly number[] => {
   return menus.map((menu) => menu.id)
 }
 
-export const getMenuEntries = (id: number, platform: number): readonly any[] => {
-  const menu = menus.find((item) => item.id === id)
-  if (!menu) {
-    return []
+export const getMenuEntries = async (id: number, platform: number): Promise<readonly MenuEntry[]> => {
+  if (id === MenuEntryId.TitleBar) {
+    return MenuEntriesTitleBar.getMenuEntries(platform)
   }
-  return menu.getMenuEntries(platform)
+  return getTitleBarMenuEntries(id, platform)
 }
